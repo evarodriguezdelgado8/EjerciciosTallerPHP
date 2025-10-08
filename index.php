0c1a07a6c1cea1f3b2c50c78c5b17cc6fd51f96f
Eva.Rodriguez <evarodriguez.24@campuscamara.es>
2025-10-08 09:43:26 +0200

    cambio1

---
+++ index.php
@@ -0,0 +1,22 @@
+//EJERCICIOS Taller PHP 
+
+<?php
+    //Ejercicio 1 SINTAXIS PHP
+    //Declare una variable llamada $nombre y asígnale tu nombre.
+
+        $nombre = "Eva"; //Declaro una variable y le asigno mi nombre
+
+    //Imprima un saludo utilizando la función echo como: "Hola, mi nombre es [tu nombre]".
+
+        echo "Hola, mi nombre es Eva";
+
+    //Define una constante llamada PI con el valor 3.1416 y muestra su valor en pantalla.
+
+        define("PI", 3.1416); // Defino la constante PI con el valor 3.1416
+        echo PI; // Imprime 3.1416
+
+    //Prueba con dos variables cuyos nombres solo difieran en mayúsculas y demuestra que PHP es sensible a mayúsculas y minúsculas.
+
+        $nombre = "Eva";
+        $Nombre = "Cristina"; // Diferente a $nombre
+?>
