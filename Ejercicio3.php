82fad9d69811161207b6dd694f5d924fd36f67e7
Eva.Rodriguez <evarodriguez.24@campuscamara.es>
2025-10-08 14:41:54 +0200

    ejejrcio 3 casi

--- Ejercicio3.php
+++ Ejercicio3.php
@@ -1,24 +1,46 @@
 <?php
-   //Ejercicio 3
-   //Defina dos variables con valores numéricos y realice operaciones aritméticas básicas (suma, resta, multiplicación, división, módulo).
+    //Ejercicio 3
+    //Defina dos variables con valores numéricos y realice operaciones aritméticas básicas (suma, resta, multiplicación, división, módulo).
 
+        $num1 = 10;
+        $num2 = 5;
 
+        echo $num1 + $num2; // Imprime 15
+        echo "<br>"; //Salto de linea
+        echo $num1 - $num2; // Imprime 5
+        echo "<br>";
+        echo $num1 * $num2; // Imprime 50
+        echo "<br>";
+        echo $num1 / $num2; // Imprime 2
+        echo "<br>";
+        echo $num1 % $num2; // Imprime 0 (resto de la división)
+        echo "<br>";
 
-   //Use operadores de comparación para verificar si una variable es mayor, menor o igual que la otra.
+    //Use operadores de comparación para verificar si una variable es mayor, menor o igual que la otra.
+
+        echo $num1 > $num2; // Imprime 1
+        echo "<br>";
+        echo $num1 < $num2; // No imprime nada
+        echo "<br>";
+        echo $num1 == $num2; // Ni imprime nada   
+        echo "<br>";
+
+    //Usa un operador ternario para determinar si una variable es par o impar.
+
+        $numero = 7;
+        $resultado = ($numero % 2 == 0) ? "par" : "impar"; // Operador ternario para comprobar si es par o impar
+
+        echo "El número $numero es $resultado"; // Mostrar el resultado
    
-   
+    // Aplica operadores lógicos para verificar si ambas variables son mayores de 10 y muestra un mensaje dependiendo del resultado.
 
-   //Usa un operador ternario para determinar si una variable es par o impar.
-
-
-   
-   // Aplica operadores lógicos para verificar si ambas variables son mayores de 10 y muestra un mensaje dependiendo del resultado.
+        
 
 
-   //Implementa operadores de incremento y decremento y muestra cómo cambian los valores de las variables.
+    //Implementa operadores de incremento y decremento y muestra cómo cambian los valores de las variables.
 
 
 
-   
+
    
 ?>
